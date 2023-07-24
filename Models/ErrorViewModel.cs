17fa4f61d53562eff80a4a50bac6e5717a932400
ib-rim <ib.bashir@hotmail.com>
2023-07-24 20:38:31 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace hiring_platform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
